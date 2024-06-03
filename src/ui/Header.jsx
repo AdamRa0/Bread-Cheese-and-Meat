c546130a0608b36b28ef3444e4ebd1f66b23c2be
Adam Rao <adamokoth@protonmail.com>
2024-06-03 09:26:33 +0300

    chore: begin styling pages

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/"> Bread, Cheese &amp; Meat </Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="tracking-widest">
+        {' '}
+        Bread, Cheese &amp; Meat{' '}
+      </Link>
       <SearchOrder />
-      <p>Admin</p>
+      <Username />
     </header>
   );
 }
